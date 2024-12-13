5ad50d291eb9920a97bf19c8318c03964dd330f0
asyiium <adl21@yandex.ru>
2024-12-13 17:08:48 +0300

    simple database test (questions and choice)

--- home/views.py
+++ home/views.py
@@ -1,8 +1,30 @@
+
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+
+from home.models import Question
 
 
 def index(request):
-    return HttpResponse("Здравствуйте здесь скоро будет сайт ЧГК не расходитесь")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list" : latest_question_list,}
+    
+    return render(request, "home/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Никаких вопросов нет.")
+
+    response = "Вопрос: \n %s"
+    return render(request, "home/index.html", {"question": question } )
+
+def vote(request, question_id):
+    return HttpResponse("Варианты ответов: %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("YAY AYAYA AY", question_id)
