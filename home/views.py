3971da984dee6e9ce942fcc3cd4809434ee5073d
Alla Elisova <fluuffyer@yandex.ru>
2024-12-14 16:50:20 +0300

    fix server

--- home/views.py
+++ home/views.py
@@ -4,15 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-
-from home.models import Question
+from .models import Question
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("pub_date")[:2]
     context = {"latest_question_list" : latest_question_list,}
-    
-    return render(request, "home/index.html", context)
+    template = loader.get_template('details.html')
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     try:
