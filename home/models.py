205351e6b807de48b4c9366375e2ad2ad0e21ec4
Alla Elisova <fluuffyer@yandex.ru>
2024-12-19 02:42:19 +0300

    upd start page

--- home/models.py
+++ home/models.py
@@ -8,7 +8,6 @@ class Question(models.Model):
 
     question_text = models.CharField(max_length = 200)
     answer_text = models.CharField(max_length = 200)
-
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
