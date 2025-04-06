1adaf592cae8570162b7231051e76a65b72fbcb3
Alla Elisova <fluuffyer@yandex.ru>
2025-04-06 23:17:41 +0300

    upd lobby

--- front_end/src/components/LobbyNavBar.js
+++ front_end/src/components/LobbyNavBar.js
@@ -19,58 +19,55 @@ const LobbyNavBar = () => {
         boxShadow: 'none', 
         zIndex: theme.zIndex.drawer + 1,
         overflow: "hidden",
-      }}
-    >
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography
-          variant="h6"
-          sx={{
-            fontWeight: 'bold',
-            color: theme.palette.text.primary,
-            cursor: 'pointer',
-          }}
-          onClick={() => handleNavigate('/')}
-        >
-          CHGK Site
-        </Typography>
-
-        <Box sx={{ display: 'flex', gap: theme.spacing(2) }}>
-          <Button
-            onClick={() => handleNavigate('/registration')}
+      }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography
+            variant="h6"
             sx={{
-              backgroundColor: theme.palette.primary.main,
-              color: theme.palette.primary.contrastText,
-              textTransform: 'none',
               fontWeight: 'bold',
-              width: '120px',
-              height: '35px',
-              '&:hover': {
-                backgroundColor: theme.palette.primary.hover,
-              },
-              borderRadius: theme.components?.MuiButton?.styleOverrides?.root?.borderRadius || 10,
+              color: theme.palette.text.primary,
+              cursor: 'pointer',
+              margin: 1,
             }}
-          >
-            Регистрация
-          </Button>
-          <Button
-            onClick={() => handleNavigate('/login')}
-            sx={{
-              backgroundColor: theme.palette.primary.main,
-              color: theme.palette.primary.contrastText,
-              textTransform: 'none',
-              fontWeight: 'bold',
-              width: '100px',
-              height: '35px',
-              '&:hover': {
-                backgroundColor: theme.palette.primary.hover,
-              },
-              borderRadius: theme.components?.MuiButton?.styleOverrides?.root?.borderRadius || 10,
-            }}
-          >
-            Войти
-          </Button>
-        </Box>
-      </Toolbar>
+            onClick={() => handleNavigate('/')}
+            >
+              CHGK Site
+          </Typography>
+          <Box sx={{ display: 'flex', gap: theme.spacing(1.8) , mr: 1}}>
+            <Button
+              onClick={() => handleNavigate('/registration')}
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.primary.contrastText,
+                textTransform: 'none',
+                fontWeight: 'bold',
+                width: '120px',
+                height: '35px',
+                '&:hover': {
+                  backgroundColor: theme.palette.primary.hover,
+                },
+                borderRadius: theme.components?.MuiButton?.styleOverrides?.root?.borderRadius || 10,
+              }}>
+                Регистрация
+            </Button>
+            <Button
+              onClick={() => handleNavigate('/login')}
+              sx={{
+                backgroundColor: theme.palette.primary.main,
+                color: theme.palette.primary.contrastText,
+                textTransform: 'none',
+                fontWeight: 'bold',
+                width: '100px',
+                height: '35px',
+                '&:hover': {
+                  backgroundColor: theme.palette.primary.hover,
+                },
+                borderRadius: theme.components?.MuiButton?.styleOverrides?.root?.borderRadius || 10,
+              }}>
+                Войти
+            </Button>
+          </Box>
+        </Toolbar>
     </AppBar>
   );
 };
--- front_end/src/pages/Lobby.js
+++ front_end/src/pages/Lobby.js
@@ -49,284 +49,284 @@ const Lobby = () => {
         position: "absolute",
         top: 0,
         left: 0,
-        width: "99vw",
-        margin: 0,
+        marginTop: 1.2,
         padding: 0,
-        overflowX: "hidden",
+        overflow: "hidden",
         backgroundColor: theme.palette.background.default,
         scrollBehavior: "smooth",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: theme.palette.background.default,
-        }}
-      >
+      }}>
         <Box
           sx={{
-            width: "75vw",
-            height: "80vh",
-            backgroundImage: "url(/big_owl.jpg)",
-            backgroundSize: "100%",
-            backgroundPosition: "center",
-            borderRadius: "40px",
-            boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.5)",
-          }}
-        ></Box>
-
-        <Box
-          sx={{
-            textAlign: "left",
-            color: theme.palette.primary.contrastText,
-            position: "absolute",
-            left: theme.spacing(27),
-            top: theme.spacing(37),
-          }}
-        >
-          <Typography
-            variant="h2"
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "1380px",
+            height: "680px",
+            backgroundColor: theme.palette.background.default,
+          }}>
+          <Box
             sx={{
-              fontWeight: "bold",
-              fontSize: "4rem",
-            }}
-          >
-            Ваш сайт
-          </Typography>
-          <Typography
-            variant="h2"
+              width: "1040px",
+              height: "550px",
+              backgroundImage: "url(/big_owl.jpg)",
+              backgroundSize: "100%",
+              backgroundPosition: "center",
+              borderRadius: "40px",
+              boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.5)",
+            }}/>
+          <Box
             sx={{
-              fontWeight: "bold",
-              fontSize: "4rem",
-              ml: theme.spacing(24),
-            }}
-          >
-            для соревнований
-          </Typography>
-          <Typography
-            variant="h2"
-            sx={{
-              fontWeight: "bold",
-              fontSize: "4rem",
-              ml: theme.spacing(80),
-            }}
-          >
-            по <span style={{ color: theme.palette.primary.main }}>ЧГК</span>
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{
-              fontSize: "1.3rem",
-              mt: 2,
-            }}
-          >
-            Последние обновления доступны прямо здесь.{" "}
-            <Button
-              onClick={scrollToNext}
-              sx={{
-                textDecoration: "underline",
-                color: theme.palette.text.primary,
-                fontSize: "1.3rem",
-                padding: 0,
-                background: "none",
-                minWidth: "auto",
-              }}
-            >
-              Узнать больше
-            </Button>
-          </Typography>
+              textAlign: "left",
+              color: theme.palette.primary.contrastText,
+              position: "absolute",
+              left: theme.spacing(27),
+              top: theme.spacing(37),
+            }}>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "4rem",
+               }}>
+                  Ваш сайт
+              </Typography>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "4rem",
+                  ml: theme.spacing(24),
+                }}>
+                  для соревнований
+              </Typography>
+              <Typography
+                variant="h2"
+                sx={{
+                  fontWeight: "bold",
+                  fontSize: "4rem",
+                  ml: theme.spacing(80),
+                }}>
+                  по <span style={{ color: theme.palette.primary.main }}>ЧГК</span>
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  fontSize: "1.3rem",
+                  mt: 2,
+                }}>
+                  Последние обновления доступны прямо здесь.{" "}
+                  <Button
+                    onClick={scrollToNext}
+                    sx={{
+                      textDecoration: "underline",
+                      color: theme.palette.text.primary,
+                      fontSize: "1.3rem",
+                      minWidth: "auto",
+                    }}>
+                      Узнать больше
+                  </Button>
+               </Typography>
         </Box>
-      </Box>
+    </Box>
 
-
-      <Box
-  id="next-section"
-  sx={{
-    display: "flex",
-    flexDirection: "column",
-    //justifyContent: "flex-start",
-    alignItems: "center",
-    width: "100vw",
-    height: "100vh",
-    padding: theme.spacing(4),
-    backgroundColor: theme.palette.background.paper,
-    position: "relative", 
-  }}
->
-
-  <Box
-    sx={{
-      width: "265px",
-      height: "218px",
-      backgroundImage: "url(/arrow.png)",
-      backgroundSize: "contain",
-      backgroundPosition: "center",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "absolute", 
-      zIndex: 1,
-      ml: 2,
-      mt: 5,
-      overflow: "visible",
-    }}
-  ></Box>
-
-  <Box
-    sx={{
-      width: "62vw",
-      height: "50vh",
-      backgroundImage: "url(/birds.png)",
-      backgroundSize: "95%",
-      backgroundPosition: "center",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      position: "relative", 
-      zIndex: 2,
-      marginBottom: 6.3,
-    }}
-  ></Box>
-
-  <Typography
-    variant="h4"
-    sx={{
-      color: theme.palette.text.primary,
-      fontWeight: "bold",
-      marginBottom: theme.spacing(0),
-    }}
-  >
-    ЧГК Рейтинг
-  </Typography>
-
-  <Typography
-    variant="body1"
-    sx={{
-      textAlign: "center",
-      marginBottom: theme.spacing(1),
-      color: theme.palette.text.secondary,
-    }}
-  >
-    “Что? Где? Когда?” — советская и российская интеллектуальная игра. <br />
-    Интеллектуальный клуб телевидения «знатоков».
-  </Typography>
-
-  <Box
-    sx={{
-      display: "grid",
-      gridTemplateColumns: "1fr 1fr",
-      gap: theme.spacing(2.5),
-      justifyItems: "center",
-      marginBottom: 10.5,
-    }}
-  >
-    {["СОСТАВЛЯЙТЕ СВОИ ВОПРОСЫ", 
-    "СОЗДАВАЙТЕ ПАКЕТЫ", 
-    "СОРЕВНУЙТЕСЬ", 
-    "ИЩИТЕ КОМАНДЫ"].map((text, idx) => (
-      <Box
-        key={idx}
-        onClick={() => navigate("/login")}
-        sx={{
-          width: "13vw",
-          height: "13vh",
-          display: "flex",
-          justifyContent: "center",
-          padding: 1,
-          alignItems: "center",
-          borderRadius: "20px",
-          boxShadow: "-4px 7px 0px 0px rgba(154, 14, 14, 1)",
-          backgroundColor: theme.palette.background.light,
-          cursor: "pointer",
-        }}
-      >
-        <Typography
-          variant="body1"
+    {/*второй экран*/}
+    <Box
+      id="next-section"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "1380px",
+        height: "680px",
+        overflow: "hidden",
+        padding: 0,
+        backgroundColor: theme.palette.background.paper,
+        position: "relative", 
+      }}>
+        <Box
           sx={{
-            fontSize: "1rem",
-            textAlign: "center",
-            padding: theme.spacing(2),
+            width: "265px",
+            height: "218px",
+            backgroundImage: "url(/arrow.png)",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "absolute", 
+            zIndex: 1,
+            mb: 42,
+          }}/>
+        <Box
+          sx={{
+            width: "925px",
+            height: "260px",
+            backgroundImage: "url(/birds.png)",
+            backgroundSize: "95%",
+            backgroundPosition: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative", 
+            zIndex: 2,
+            marginBottom: 4,
+            marginTop: 0.5,
+          }}/>
+        <Typography
+          variant="h4"
+          sx={{
             color: theme.palette.text.primary,
             fontWeight: "bold",
-          }}
-        >
-          {text}
-        </Typography>
-      </Box>
-    ))}
-  </Box>
-</Box>
-
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw",
-          height: "100vh",
-          padding: theme.spacing(4),
-          backgroundColor: theme.palette.background.default,
-        }}
-      >
-        <Typography
-          variant="h3"
-          sx={{
-            color: theme.palette.primary.main,
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
-        >
-          Третий экран
+            marginBottom: theme.spacing(0),
+          }}>
+            ЧГК Рейтинг
         </Typography>
         <Typography
           variant="body1"
           sx={{
             textAlign: "center",
-            marginTop: theme.spacing(2),
-            color: theme.palette.text.primary,
-          }}
-        >
-          Добро пожаловать на третий экран! Здесь может быть ваш контент.
+            marginBottom: theme.spacing(1),
+            color: theme.palette.text.secondary,
+          }}>
+            “Что? Где? Когда?” — советская и российская интеллектуальная игра. <br />
+            Интеллектуальный клуб телевидения «знатоков».
         </Typography>
-      </Box>
 
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+            gap: theme.spacing(2.5),
+            marginBottom: theme.spacing(8),
+            justifyItems: "center",
+          }}>
+            {["СОСТАВЛЯЙТЕ СВОИ ВОПРОСЫ", 
+            "СОЗДАВАЙТЕ ПАКЕТЫ", 
+            "СОРЕВНУЙТЕСЬ", 
+            "ИЩИТЕ КОМАНДЫ"].map((text, idx) => (
+              <Box
+                key={idx}
+                onClick={() => navigate("/login")}
+                sx={{
+                  width: "200px",
+                  height: "100px",
+                  display: "flex",
+                  justifyContent: "center",
+                  padding: 0,
+                  alignItems: "center",
+                  borderRadius: "20px",
+                  boxShadow: "-4px 7px 0px 0px rgba(154, 14, 14, 1)",
+                  backgroundColor: theme.palette.background.light,
+                  cursor: "pointer",
+                }}>
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      fontSize: "1rem",
+                      textAlign: "center",
+                      color: theme.palette.text.primary,
+                      fontWeight: "bold",
+                    }}>
+                      {text}
+                  </Typography>
+              </Box> 
+            ))}
+        </Box>
+    </Box>
+
+    {/*третий экран*/}
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "left",
+        alignItems: "center",
+        width: "1380px",
+        height: "540px",
+        overflow: "hidden", 
+        backgroundColor: theme.palette.background.default,
+        marginLeft: 9,
+        marginBottom: 5,
+      }}>
       <Box
-        onClick={scrollToTop}
         sx={{
-          position: "fixed",
-          bottom: theme.spacing(4),
-          right: theme.spacing(4),
-          width: "50px",
-          height: "50px",
-          backgroundColor: showButton
-            ? isClicked
-              ? theme.palette.text.disabled
-              : theme.palette.common.white
-            : theme.palette.text.disabled,
-          borderRadius: "50%",
-          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+          width: "600px",
+          height: "520px",
+          backgroundImage: "url(/side_owl.jpg)",
+          backgroundSize: "95%",
+          borderRadius: "40px",
+          boxShadow: "0px 4px 15px rgba(0, 0, 0, 0.5)",
+          backgroundPosition: "left",
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          cursor: "pointer",
-          zIndex: 1000,
-        }}
-      >
+          justifyContent: "left",
+          position: "relative",
+          zIndex: 2,
+        }}/>
+      <Box
+        sx={{
+          color: theme.palette.primary.contrastText,
+          ml: 5, 
+          width: "40%", 
+        }}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: "1.5rem",
+              textAlign: "left",
+              mb: 8,
+            }}>
+              ЧГК - это не только интеллектуальное соревнование, но и увлечение, развивающее командный дух. <br />
+              Найдите себя и своих друзей в нашем коммьюнити сегодня!
+          </Typography>
+          <Button
+            onClick={() => navigate('/login')}
+            sx={{
+              backgroundColor: theme.palette.primary.main,
+              color: theme.palette.primary.contrastText,
+              fontSize: "2.4rem",
+              width: '450px',
+              height: '80px',
+              borderRadius: "50px",
+              marginBottom: 8,
+              zIndex: 4,
+              '&:hover': {
+                backgroundColor: theme.palette.primary.hover,
+              },
+              borderRadius: theme.components?.MuiButton?.styleOverrides?.root?.borderRadius || 10,
+            }}>
+              Попробовать
+          </Button>
+      </Box>
+    </Box>
+
+
+    {/*стрелка*/}
+    <Box
+      onClick={scrollToTop}
+      sx={{
+        position: "fixed",
+        bottom: theme.spacing(4),
+        right: theme.spacing(4),
+        width: "50px",
+        height: "50px",
+        backgroundColor: showButton ? isClicked ? theme.palette.text.disabled : theme.palette.common.white : theme.palette.text.disabled,
+        borderRadius: "50%",
+        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        cursor: "pointer",
+        zIndex: 10,
+      }}>
         <ArrowUpwardIcon
           sx={{
             color: theme.palette.primary.main,
             fontSize: "24px",
-          }}
-        />
-      </Box>
+          }}/>
     </Box>
-  );
-};
+  </Box> );
+  
+  };
 
 export default Lobby;
